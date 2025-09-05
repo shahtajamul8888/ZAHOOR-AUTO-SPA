f23f8cd577d97f048d9be5fe8fbc9196c10bc097
shahtajamul8888 <shahtajamul8888@gmail.com>
2025-09-05 20:46:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-function copyLink() {
-  navigator.clipboard.writeText("https://tinyurl.com/ZahoorAutoSpa");
-  let toast = document.getElementById("toast");
-  toast.classList.add("show");
-  setTimeout(() => { toast.classList.remove("show"); }, 2000);
+function bookNow() {
+  alert("Booking service coming soon!");
 }
